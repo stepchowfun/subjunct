44b6a3d9c0764c358a85396dc2b669a8739e8d12
Stephan Boyer <stephan@stephanboyer.com>
2013-11-06 13:30:01 -0500

    basic homepage styles

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Subjunct::Application.routes.draw do
+  root to: 'home#index'
+  get '/about', to: "home#about"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
