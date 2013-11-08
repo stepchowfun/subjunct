dd45455a98617e38b83875ffcc1529921d02544b
Stephan Boyer <stephan@stephanboyer.com>
2013-11-08 04:24:16 -0500

    now using https, and some style updates

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,21 @@
 class HomeController < ApplicationController
+  # ensure that HTTPS is used
+  before_filter :use_https
+
   def index
   end
 
   def about
+  end
+
+private
+  # make sure we are using https
+  # used as a before filter
+  def use_https
+    if Rails.env.production?
+      if request.protocol != "https://"
+        return redirect_to "https://#{request.url[(request.protocol.size)..(-1)]}"
+      end
+    end
   end
 end
